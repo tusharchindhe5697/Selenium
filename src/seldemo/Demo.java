198a1be653db3758effcff1c2ca0342f3eee1610
user <user@DESKTOP-I753C4I>
2021-12-27 16:42:15 +0530

    My first try

---
+++ src/seldemo/Demo.java
@@ -0,0 +1,14 @@
+package seldemo;
+
+public class Demo {
+	public static void main(String[] args) {
+	
+		Demo.demo1();
+	}
+	
+	public static void demo1() {
+		
+		System.out.println("Hello, this is my first github repository");
+		
+	}
+}
